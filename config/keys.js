1955454038cb0ef3f32ab69e0e0035105a0b28ab
Spencer Marx <spencer.s.marx@gmail.com>
2019-09-17 17:07:27 -0400

    Restructure server directories for heroku deploy

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// ======================================
+// Export
+// ======================================
+module.exports = {
+    mongoURI: 'mongodb+srv://spencermarx:Angie37477842.@project-apps-tpbj7.mongodb.net/todo_mern_app?retryWrites=true&w=majority',
+    secretOrKey: "tomato93"
+};
